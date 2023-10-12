4cc8062486fcb47f78b3d9691a08c668c570b8c9
= <=>
2023-10-13 00:07:24 +0800

    section kelas is done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
+import { kelasTerbaru } from "../data/index";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  let navigate = useNavigate();
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
@@ -29,7 +32,56 @@ const HomePage = () => {
           </Row>
         </Container>
       </header>
-      <div className="kelas w-100 min-vh-100"></div>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Waifu terbaru</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas) => {
+              return (
+                <Col key={kelas.id}>
+                  <img
+                    src={kelas.image}
+                    alt="unplash.com"
+                    className="w-100 rounded-top mb-5"
+                  />
+                  <div className="star mb-2 px-3">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex justify-content-between alig-align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button
+                className="btn btn-success rounded-5 btn-lg"
+                onClick={() => navigate("/kelas")}
+              >
+                Lihat semua waaifu
+                <i className="fa-solid  fa-chevron-right ms-1"></i>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
