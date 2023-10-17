f62141a5434a9e8504d4d715532d86495b8471d5
= <=>
2023-10-17 11:37:15 +0800

    end homepage testimonial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
-import { kelasTerbaru } from "../data/index";
+import { kelasTerbaru, dataSwiper } from "../data/index";
 import { useNavigate } from "react-router-dom";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const HomePage = () => {
   let navigate = useNavigate();
@@ -9,7 +19,7 @@ const HomePage = () => {
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
-          <Row className="header-box d-flex align-items-center">
+          <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6" className="pt-lg-0 pt-5">
               <h1 className="mb-4">
                 Ikuzooo <br /> <span>Cari tahu Waifumu</span> <br /> bersama
@@ -45,7 +55,7 @@ const HomePage = () => {
           <Row>
             {kelasTerbaru.map((kelas) => {
               return (
-                <Col key={kelas.id}>
+                <Col key={kelas.id} className="shadow">
                   <img
                     src={kelas.image}
                     alt="unplash.com"
@@ -79,6 +89,59 @@ const HomePage = () => {
                 <i className="fa-solid  fa-chevron-right ms-1"></i>
               </button>
             </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold my-5">Testimonial</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
+                992: {
+                  slidesPerView: 2,
+                  spaceBetween: 50,
+                },
+                1200: {
+                  slidesPerView: 3,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {dataSwiper.map((data) => {
+                return (
+                  <SwiperSlide key={data.id} className="shadow-sm">
+                    <p className="desc">{data.desc}</p>
+                    <div className="people">
+                      <img src={data.image} alt="" />
+                      <div>
+                        <h5 className="mb-1">{data.name}</h5>
+                        <p className="m-0 fw-bold">{data.skill}</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </Row>
         </Container>
       </div>
